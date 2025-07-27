f3b21025ec67e634820d27c9f18836f63c4fbeca
Abdessamad El Allali <abel-all@e3r2p16.1337.ma>
2025-07-27 22:42:00 +0100

    feat: create a Abel42 token smart contract

---
+++ code/Abel42.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
+
+contract Abel42 is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Abel42", "TKN42") {
+        _mint(msg.sender, initialSupply * (10**18));
+    }
+}
